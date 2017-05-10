19ea1aec511e7eb5a2d1a03602c49bb81c200840
GammaRay2033 <maxcrz2033@gmail.com>
2017-05-10 12:36:46 -0700

    Add files via upload

---
+++ Practica5.cpp
@@ -0,0 +1,16 @@
+#include <mraa.h>
+#include <stdio.h>
+#include <cstdlib>
+
+int main(void){
+	char c;
+	puts("Enter text '.' to exit");
+	do{
+		system("/bin/stty raw");
+		c=getchar();
+		putchar(c);
+		system("/bin/stty -raw");
+	}while(c!='.');
+	return 0;
+}
+
