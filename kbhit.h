a27e950ffb1a458479dd98704125ffd66d7b366e
GammaRay2033 <maxcrz2033@gmail.com>
2017-05-23 16:50:10 -0700

    Update kbhit.h

--- kbhit.h
+++ kbhit.h
@@ -1,22 +1,9 @@
 #ifndef KBHITh
 #define KBHITh
 
-#include <termios.h>
+void   init_keyboard(void);
+void   close_keyboard(void);
+int      kbhit(void);
+int     readch(void); 
 
-class keyboard
-{
-public:
-
-      keyboard();
-    ~keyboard();
-    int kbhit();
-    int getch();
-
-private:
-
-    struct termios initial_settings, new_settings;
-    int peek_character;
-
-};
-
-#endif 
+#endif
