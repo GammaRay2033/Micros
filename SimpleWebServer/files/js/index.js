1d7d619b0288136c9a9e990ce6512f7435bfd5cd
GammaRay2033 <maxcrz2033@gmail.com>
2017-06-07 16:54:39 -0700

    Update index.js

--- SimpleWebServer/files/js/index.js
+++ SimpleWebServer/files/js/index.js
@@ -1,14 +1,14 @@
-var ctx = document.getElementById('chart').getContext('2d');
+var ctx = document.getElementById("chart").getContext("2d");
 var myChart = new Chart(ctx, {
   type: 'line',
   data: {
-    labels: ['M', 'T', 'W', 'T', 'F', 'S', 'S'],
+    labels: ["M", "T", "W", "T", "F", "S", "S"],
     datasets: [{
-      label: 'apples',
+      label: "apples",
       data: [12, 19, 3, 17, 6, 3, 7],
       backgroundColor: "rgba(153,255,51,0.6)"
     }, {
-      label: 'oranges',
+      label: "oranges",
       data: [2, 29, 5, 5, 2, 3, 10],
       backgroundColor: "rgba(255,153,0,0.6)"
     }]
