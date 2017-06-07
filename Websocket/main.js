12ed53d7a2d49cb776dd6f628490e5e0b44a9dd3
GammaRay2033 <maxcrz2033@gmail.com>
2017-06-07 14:11:29 -0700

    Update main.js

--- Websocket/main.js
+++ Websocket/main.js
@@ -1,1 +1,16 @@
+// Load Grove module
+var TH02 = require('th02js');
 
+// Create the temperature sensor object using AIO pin 0
+var bus = 6;
+var sensor = new TH02(bus);
+
+// Read the temperature ten times, printing both the Celsius and
+// equivalent Fahrenheit temperature, waiting one second between readings
+var i = 0;
+var waiting = setInterval(function() {
+        var celsius = sensor.getCelsiusTemp();
+        console.log(celsius + " degrees Celsius");
+        i++;
+        if (i == 10) clearInterval(waiting);
+        }, 1000);
