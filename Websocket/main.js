f20a2e2c02e1238c487746cc96f7d0073167a6b9
GammaRay2033 <maxcrz2033@gmail.com>
2017-06-07 20:19:09 -0700

    Create main.js

--- Websocket/main.js
+++ Websocket/main.js
@@ -15,7 +15,6 @@ app.get('/', function(req, res) {
 io.on('connection', function(socket){
     var interval = setInterval(function(){
     socket.emit('temperature', {celsius: Math.round(sensor.getCelsiusTemp()*Math.pow(10,2))/Math.pow(10,2)});
-    console.log(sensor.getCelsiusTemp());
     }, 500); //Read the temperature every 500ms and send the reading
     socket.on('disconnect', function(){
         clearInterval(interval);
