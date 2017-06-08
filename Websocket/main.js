5f4a26da0b756c2617a2b16c07746f42df89f717
GammaRay2033 <maxcrz2033@gmail.com>
2017-06-07 20:59:46 -0700

    Create main.js

--- Websocket/main.js
+++ Websocket/main.js
@@ -1,4 +1,5 @@
 var TH02 = require('th02js'); //TH02js Library
+var mraa = require('mraa'); //MRAA Library
 var app = require('express')(); //Express Library
 var server = require('http').Server(app); //Create HTTP instance
 var io = require('socket.io')(server); //Socket.IO Library
@@ -6,7 +7,9 @@ var io = require('socket.io')(server); //Socket.IO Library
 server.listen(3000); //Run on port 3000
 
 var bus = 6;
-var sensor = new TH02(bus); // Create the temperature sensor object
+var th02Sensor = new TH02(bus); // Create th02 sensor object
+
+var lightSensor = new mraa.Aio(0); //Create light sensor object
 
 app.get('/', function(req, res) {                  
     res.sendFile(__dirname + '/index.html'); //Serve the html file
@@ -15,9 +18,9 @@ app.get('/', function(req, res) {
 io.on('connection', function(socket){
     var interval = setInterval(function(){
     socket.emit('ambient', {
-        temperature: Math.round(sensor.getCelsiusTemp()*Math.pow(10,2))/Math.pow(10,2),
-        humidity: Math.round(sensor.getHumidity()*Math.pow(10,2))/Math.pow(10,2),
-        lighting: 35
+        temperature: Math.round(th02Sensor.getCelsiusTemp()*Math.pow(10,2))/Math.pow(10,2),
+        humidity: Math.round(th02Sensor.getHumidity()*Math.pow(10,2))/Math.pow(10,2),
+        lighting: lightSensor.read();
     });
     }, 500); //Read the temperature every 500ms and send the reading
     socket.on('disconnect', function(){
