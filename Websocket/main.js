e9e0e335bb4e442d272bb8092d38fcdc88bc12ba
GammaRay2033 <maxcrz2033@gmail.com>
2017-06-07 19:15:10 -0700

    Create main.js

--- Websocket/main.js
+++ Websocket/main.js
@@ -1,12 +1,14 @@
 var TH02 = require('th02js'); //TH02js Library
+var m = require('mraa'); //MRAA Library 
 var app = require('express')(); //Express Library
 var server = require('http').Server(app); //Create HTTP instance
 var io = require('socket.io')(server); //Socket.IO Library
 
 server.listen(3000); //Run on port 3000
 
-var bus = 12;
-var sensor = new TH02(bus); // Create the temperature sensor object
+var bus = 6;
+let i2cDevice = new mraa.I2c(0);
+var sensor = new TH02(i2cDevice); // Create the temperature sensor object
 
 app.get('/', function(req, res) {                  
     res.sendFile(__dirname + '/index.html'); //Serve the html file
