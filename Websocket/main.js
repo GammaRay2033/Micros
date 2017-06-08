3bd895b2178102e06245d8f8e7867d8ac1610f6e
GammaRay2033 <maxcrz2033@gmail.com>
2017-06-07 21:01:43 -0700

    Update main.js

--- Websocket/main.js
+++ Websocket/main.js
@@ -20,7 +20,7 @@ io.on('connection', function(socket){
     socket.emit('ambient', {
         temperature: Math.round(th02Sensor.getCelsiusTemp()*Math.pow(10,2))/Math.pow(10,2),
         humidity: Math.round(th02Sensor.getHumidity()*Math.pow(10,2))/Math.pow(10,2),
-        lighting: lightSensor.read();
+        lighting: lightSensor.read()
     });
     }, 500); //Read the temperature every 500ms and send the reading
     socket.on('disconnect', function(){
