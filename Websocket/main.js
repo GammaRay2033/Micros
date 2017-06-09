3abb1e79cf379a80b1f34274a828b12243921bed
GammaRay2033 <maxcrz2033@gmail.com>
2017-06-08 17:15:50 -0700

    Create main.js

--- Websocket/main.js
+++ Websocket/main.js
@@ -1,8 +1,15 @@
 var TH02 = require('th02js'); //TH02js Library
 var mraa = require('mraa'); //MRAA Library
-var app = require('express')(); //Express Library
+var express = require('express'); //Express Library
+var app = express();
 var server = require('http').Server(app); //Create HTTP instance
 var io = require('socket.io')(server); //Socket.IO Library
+
+app.use(express.static(__dirname+'/files'));
+
+app.get('/', function(req, res) {                  
+    res.send('Hello World!'); //Serve the html file
+});
 
 server.listen(3001); //Run on port 3001
 
@@ -10,10 +17,6 @@ var bus = 6;
 var th02Sensor = new TH02(bus); // Create th02 sensor object
 
 var lightSensor = new mraa.Aio(0); //Create light sensor object
-
-app.get('/', function(req, res) {                  
-    res.sendFile(__dirname + '/index.html'); //Serve the html file
-});
 
 io.on('connection', function(socket){
     var interval = setInterval(function(){
