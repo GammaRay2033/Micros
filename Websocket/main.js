f56be2f0ab24d9b71b55a8b79021beba0883f228
GammaRay2033 <maxcrz2033@gmail.com>
2017-06-09 01:23:54 -0700

    Create main.js

--- Websocket/main.js
+++ Websocket/main.js
@@ -3,12 +3,23 @@ var mraa = require('mraa'); //MRAA Library
 var app = require('express')(); //Express Library
 var server = require('http').Server(app); //Create HTTP instance
 var io = require('socket.io')(server); //Socket.IO Library
-var bus = 6;
-var th02Sensor = new TH02(bus); // Create th02 sensor object
-var lightSensor = new mraa.Aio(0); //Create light sensor object
-var ledPin = new mraa.Gpio(13); //Set digital output in pin 13
+var jsUpmI2cLcd = require('jsupm_i2clcd'); //LCD Library
+var LcdTextHelper = require('./lcd_text_helper'); //Text sliding library
+var ip = require('ip'); //IP Address Library
+
+var th02Sensor = new TH02(6); // Create th02 sensor object
+var lightSensor = new mraa.Aio(0); //Create analog input objet in pin A0
+var ledPin = new mraa.Gpio(13); //Create GPIO object in pin 13
+ledPin.dir(mraa.DIR_OUT); //Set GPIO direction to output
 var ledState = 0; //Default LED state
-ledPin.dir(mraa.DIR_OUT); //Set the gpio direction to output
+var ipAddr = ip.address(); //Get server IP address
+var lcd = new jsUpmI2cLcd.Jhd1313m1(6, 0x3E, 0x62); //Create LCD object
+var lcdText = new LcdTextHelper(lcd); //Create LCD text handling object
+
+lcdText.set([ //Set initial text in LCD
+  "Ambient Monitor ", 
+  "    -CETYS-     "
+]);
 
 app.get('/', function(req, res) {                  
     res.sendFile(__dirname + '/files/index.html'); //serve the static html file
@@ -16,12 +27,26 @@ app.get('/', function(req, res) {
 
 server.listen(3000); //Run on port 3000
 
+setInterval(function(){
+    var tValue = Math.round(th02Sensor.getCelsiusTemp()*100)/100;
+    var hValue = Math.round(th02Sensor.getHumidity()*100)/100;
+    var lValue = Math.round(lightSensor.readFloat()*100*100)/100;
+
+    lcdText.set([
+      "Check http://" + ipAddr + ":3000 in web browser for real time monitoring", 
+      " T=" + parseInt(tValue) + "H=" + parseInt(hValue) + "L=" + parseInt(lValue) + " "
+    ]);
+}, 100);
+
 io.on('connection', function(socket){
     var interval = setInterval(function(){
+    tValue = Math.round(th02Sensor.getCelsiusTemp()*100)/100;
+    hValue = Math.round(th02Sensor.getHumidity()*100)/100;
+    lValue = Math.round(lightSensor.readFloat()*100*100)/100;
     socket.emit('ambient', {
-        temperature: Math.round(th02Sensor.getCelsiusTemp()*100)/100,
-        humidity: Math.round(th02Sensor.getHumidity()*100)/100,
-        lighting: Math.round(lightSensor.readFloat()*100*100)/100
+        temperature: tValue,
+        humidity: hValue,
+        lighting: lValue
     });
     }, 100); //Read the temperature every 500ms and send the reading
     socket.on('disconnect', function(){
